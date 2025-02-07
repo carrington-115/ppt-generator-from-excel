a9882c1902bd0ddda52218957c9ab8672a9d0710
carrington-115 <frumarkcarrington@gmail.com>
2025-02-07 22:39:46 +0530

    updated generate ppt file and read file to read multiple sheets from an excel file and transform table data to slide data

--- source/generatePPTfile.py
+++ source/generatePPTfile.py
@@ -1,17 +1,18 @@
 from pptx import Presentation
-from pptx.util import Inches
+from pptx.util import Inches, Pt
+from source.readFile import ReadExcelFile
 
 prs = Presentation()
 
 class GeneratePPTFromFile:
-    def __init__(self, file_name, excel_file_data,):
+    def __init__(self, file_name, excel_file_data, excel_file_location):
         self.file_name = file_name
         self.excel_file_data = excel_file_data
+        self.excel_file_location = excel_file_location
 
 
-    def addSlideToPresentation(self, slide_position):
-        slide_layout = prs.slide_layouts[slide_position] # blank slide
-        slide_info = prs.slide.add_slide(slide_layout)
+    def addSlideToPresentation(self, slide_layout):
+        slide_info = prs.slides.add_slide(slide_layout)
         return slide_info
     
     def enterTableData(self, rows, cols, table, sheet_data):
@@ -19,24 +20,59 @@ class GeneratePPTFromFile:
             for j in range(cols):
                 table.cell(i, j).text = str(sheet_data[i][j])
 
-    def generatePPTForSingleSheet(self, ppt_location, row_num, col_num):
+        self.editFontSizeAndFamily(rows, cols, table)
+                
+
+    def generatePPTForSingleSheet(self, ppt_location, sheet_name):
         # defined the sheet sizes
-        height = Inches(6.49)
-        width = Inches(12.48)
-        top = Inches(0.5)
-        left = Inches(0.5)
-        slide_info = self.addSlideToPresentation(5)
-        rows, cols = row_num, col_num
+        height = Inches(4.5)
+        width = Inches(9.52)
+        top = Inches(0.1)
+        left = Inches(0.1)
+        slide_info = self.addSlideToPresentation(1)
+        excel_obj = ReadExcelFile(self.excel_file_location)
+        rows, cols = excel_obj.rowAndCol(sheet_name)
         # create the table
         table_data = slide_info.shapes.add_table(rows, cols, left, top, width, height).table
         
         # get the sheet data
-        sheet_data =[[], []]
+        sheet_data = excel_obj.readDataFromSheet(sheet_name)
         self.enterTableData(rows, cols, table_data, sheet_data)
+        
         # enter slide_data
-        prs.save(ppt_location)
+        prs.save(ppt_location + f'{sheet_name}.pptx')
 
-    def sayHello(self): 
+    def useGenerate(self, sheet_data, rows, cols):
+        # defined the sheet sizes
+        height = Inches(4.5)
+        width = Inches(9.52)
+        top = Inches(1)
+        left = Inches(1)
+        slide_layout = prs.slide_layouts[5]
+        slide_info = self.addSlideToPresentation(slide_layout)
+        table_data = slide_info.shapes.add_table(rows, cols, left, top, width, height).table
+        self.enterTableData(rows, cols, table_data, sheet_data)
+        
+
+    def generatePPTForMultipleSheet(self, ppt_location):
+        read_object = ReadExcelFile(self.excel_file_location)
+        all_sheet_data = read_object.readMultipleSheetData()
+        sheet_names = read_object.sheetNames()
+
+        for name, data in all_sheet_data.items():
+            rows, cols = read_object.rowAndCol(name)
+            self.useGenerate(data, rows, cols)
+            
+
+    def editFontSizeAndFamily(self, rows, cols, table):
+        for i in rows:
+            for j in cols:
+                cell = table.cell(i, j)
+                for paragraph in cell.text_frame.paragraphs:
+                    paragraph.font.size = Pt(12)
+                    paragraph.font.name = "EYInterstate Light"
+
+    def sayHello(self):
         print('hello world')
 
     # this method is used for a print action in the class
--- source/readFile.py
+++ source/readFile.py
@@ -9,10 +9,30 @@ class ReadExcelFile:
         return pd.read_excel(excel_sheet, sheet_name).fillna(' ').to_numpy()
 
     def readDataFromSheet(self, sheet_name):
-        sheet_data = [[]]
+        sheet_data = []
         pandas_xls = pd.ExcelFile(self.file_location)
         sheet_data = self.loopThroughSheet(pandas_xls, sheet_name)
         return sheet_data
+
+    def rowAndCol(self, sheet_name):
+        data = self.readDataFromSheet(sheet_name)
+        rows, cols = len(data), len(data[0])
+        return rows, cols
+    
+    def sheetNames(self):
+        return pd.ExcelFile(self.file_location).sheet_names
+    
+    def readMultipleSheetData(self):
+        excel_file = pd.ExcelFile(self.file_location)
+        all_data = dict({})
+        for sheet in excel_file.sheet_names:
+            df = pd.read_excel(excel_file, sheet)
+            df = df.fillna(' ')
+            sheet_data = df.to_numpy()
+            all_data.setdefault(f'{sheet}', sheet_data)
+        return all_data
+
+
 
     # test print
     def __str__(self):
